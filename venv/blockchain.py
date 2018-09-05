cba31c5d496c23cfe0a91d736ed5fdcb9af715be
Christine Graff <chrisgraff@protonmail.com>
2018-09-05 12:05:41 -0400

    new block

---
+++ venv/blockchain.py
@@ -0,0 +1,67 @@
+import hashlib
+import json
+from time import time
+
+class Blockchain(object):
+
+    def __init__(self):
+        self.chain = []
+        self.currentTransactions = []
+        self.nodes = set()
+
+        #create genesis block
+        self.new_block(previous_hash=1, proof=100)
+
+    def new_block(self):
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.currentTransactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+
+        self.currentTransactions = []
+        self.chain.append(block)
+
+        return block
+
+    # Adds a new transaction to the list of transactions
+    def new_transaction(self, sender, recipient, amount):
+        """
+
+        Creates a new transaction to go into the next mined Block
+
+        :param sender: <str> Address of the Sender
+
+        :param recipient: <str> Address of the Recipient
+
+        :param amount: <int> Amount
+
+        :return: <int> The index of the Block that will hold this transaction
+
+        """
+
+        self.currentTransactions.append({
+
+            'sender': sender,
+
+            'recipient': recipient,
+
+            'amount': amount,
+
+        })
+
+        return self.last_block['index'] + 1
+
+    @staticmethod
+    def hash(block):
+        # Hashes a Block
+
+        pass
+
+    @property
+    def last_block(self):
+        # Returns the last Block in the chain
+
+        pass
