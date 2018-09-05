205a6996cdfab516108d4a4889c881a89ed4ebf3
Christine Graff <chrisgraff@protonmail.com>
2018-09-05 11:35:03 -0400

    hash test

---
+++ venv/hashtest.py
@@ -0,0 +1,15 @@
+from hashlib import sha256
+
+x = 5
+y = 0
+
+# Keep incrementing until the last 4 digits ends with a 0000.. which would yield 5 * 54455
+while sha256(f'{x*y}'.encode()).hexdigest()[:4] != "0000":
+   y += 1
+
+
+def create_hash(x, y):
+   return sha256(f'{x*y}'.encode()).hexdigest()
+
+
+print (f'The solution for y = {y} and hash is:  {create_hash(x, y)}')
